918e7f85a4375c7bd08f4007ba36809b062630c7
asifcaspi <asifcaspi1@gmail.com>
2023-06-24 22:32:19 +0300

    add connection

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCESS_KEY = os.getenv('ACCESS_KEY_ID')
+SECRET_KEY = os.getenv('SECRET_ACCESS_KEY')
+HOST = os.getenv('PGSQL_HOST')
+PORT = 5432
+DATABASE = os.getenv('PGSQL_DATABASE')
+PASSWORD = os.getenv('PGSQL_PASSWORD')
+USERNAME = os.getenv('PGSQL_USERNAME')
+BUCKET = os.getenv('BUCKET')
+REGION_NAME = 'eu-central-1'
+# ENCRYPTION_KEY = 
---
+++ db.py
@@ -0,0 +1,26 @@
+import boto3
+import psycopg2
+import config
+
+# Connect to Amazon S3
+s3 = boto3.client('s3',
+                  aws_access_key_id=config.ACCESS_KEY,
+                  aws_secret_access_key=config.SECRET_KEY, 
+                  region_name=config.REGION_NAME)
+# Connect to PostgreSQL
+conn = psycopg2.connect(
+    host=config.HOST,
+    port=config.PORT,
+    database=config.DATABASE,
+    user=config.USERNAME,
+    password=config.PASSWORD
+)
+cur = conn.cursor()
+
+
+def close_connection():
+    cur.close()
+    conn.close()
+
+
+# Close the PostgreSQL connection
